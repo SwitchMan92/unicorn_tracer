950741f6e26a428ac4a6050134c92d2846f9eabb
TOM LANGFELDT <tom.langfeldt.external@airbus.com>
2018-11-09 14:54:39 +0100

    Updating setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
                 url='https://github.com/SwitchMan92/unicorn_tracer',
                 packages=setuptools.find_packages(),
                 install_requires=['termcolor', 'unicorn'],
-                python_requires='=2.*',
+                python_requires='==2.*',
                 classifiers=[
                     "Programming Language :: Python :: 2.7",
                     "License :: OSI Approved :: GNU General Public License (GPL)",
